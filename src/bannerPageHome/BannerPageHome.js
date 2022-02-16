83aa7da6919233876c9c57cd6541865714d13583
Marcoantonio9 <marcojr918@gmail.com>
2022-02-16 13:27:50 -0300

    Update arquivos iniciais

---
+++ src/bannerPageHome/BannerPageHome.js
@@ -0,0 +1,30 @@
+import './BannerPageHome.css'
+
+const BannerPageHome = () => {
+  return (
+    <div className='banner'>
+      <div>
+        <h1>
+          Fique por dentro dos
+          <br></br>
+          próximos lançamentos!
+        </h1>
+        <p className='text-banner'>
+          O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. 
+          O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, 
+          quando uma misturou os caracteres de um texto para criar um espécime de livro.
+        </p>
+
+        <button className='btn-saiba'>
+          Saiba Mais
+        </button>
+      </div>
+
+      <div className='area-astro-img'>
+        <img src='/images/astro3.png' />
+      </div>
+    </div>
+  )
+}
+
+export default BannerPageHome
---
+++ src/header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src='/images/logo.png' />
+      </div>
+    </div>
+  )
+}
+
+export default Header
