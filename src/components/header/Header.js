503406c7d80609875ccfafa2e449ab591970083f
Marcoantonio9 <marcojr918@gmail.com>
2022-02-17 19:22:53 -0300

    Ajustes scroll navbar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,20 @@
+import React from 'react'
 import './Header.scss'
 
 const Header = () => {
+  const [navbar, setNavbar] = React.useState(false)
+
+  const changeBackground = () => {
+    if(window.scrollY >= 80){
+      setNavbar(true)
+    }else{
+      setNavbar(false)
+    }
+  }
+
+  window.addEventListener('scroll', changeBackground)
   return (
-    <div className="header">
+    <div className={navbar ? 'header active' : 'header'}>
       <div className="logo">
         <img src='/images/logo.png' />
       </div>
