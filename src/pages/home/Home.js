e1a949ee634a3bcd5fd996433eb5dce00ae94961
Marcoantonio9 <marcojr918@gmail.com>
2022-02-21 18:04:03 -0300

    Config pastas

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,6 @@
-import BannerPageHome from "./bannerPageHome/BannerPageHome"
-import NextLaunches from "./nextLaunches/NextLaunches"
-import LastLaunches from "./lastLaunches/LastLaunches"
+import BannerPageHome from "../../components/bannerPageHome/BannerPageHome"
+import NextLaunches from "../../components/nextLaunches/NextLaunches"
+import LastLaunches from "../../components/lastLaunches/LastLaunches"
 import Rockets from "../../components/rockets/Rockets"
 
 const Home = () => {
