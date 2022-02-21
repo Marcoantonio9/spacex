ef7546660e85b9436f5c7a9af1c52f9a8450902a
Marcoantonio9 <marcojr918@gmail.com>
2022-02-21 19:42:22 -0300

    Animação scroll

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Rockets.scss'
 import axios from 'axios'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Rockets = () => {
   const [rockets, setRockets] = React.useState([])
@@ -8,8 +10,8 @@ const Rockets = () => {
     const url = `https://api.spacexdata.com/v4/rockets`
 
     axios.get(url).then((res) => {
-      console.log('history: ', res)
       setRockets(res.data)
+      Aos.init({duration: 1000})
     })
   }, [])
   const stylePost = {
@@ -18,45 +20,54 @@ const Rockets = () => {
     // width: '50%'
 
   }
-  return(
+
+  return (
     <div id="rockets">
-      <h1>Foguetes:</h1>
-      {rockets.map((item, index) => (
-        <div className="area-rocket" key={index}>
-          <div className="area-infos" style={index%2 !== 0 ? stylePost : {order: 0}}>          
-            <h1>{item.name}</h1>
-            <div className="list-infos">
-              <div>
-                <p>Altura: </p>
+        <div>
+          <h1>Foguetes:</h1>
+        </div>
+        {rockets.map((item, index) => (
+          <div className="area-rocket" key={index}>
+            <div className="area-infos" 
+              style={index % 2 !== 0 ? stylePost : { order: 0 }} 
+              data-aos={index % 2 !== 0 ? "fade-right" : "fade-left"}
+            >
+              <h1>{item.name}</h1>
+              <div className="list-infos">
+                <div>
+                  <p>Altura: </p>
+                </div>
+                <div>
+                  <p>{item.height.meters}m</p>
+                </div>
               </div>
-              <div>
-                <p>{item.height.meters}m</p>
+
+              <div className="list-infos">
+                <div>
+                  <p>Peso: </p>
+                </div>
+                <div>
+                  <p>{Number(item.mass.kg).toLocaleString()} KG</p>
+                </div>
+              </div>
+              <div className="list-infos list-infos-twodisplay">
+                <div>
+                  <p>Descrição: </p>
+                </div>
+                <div>
+                  <p className="text-description">{item.description} KG</p>
+                </div>
               </div>
             </div>
 
-            <div className="list-infos">
-              <div>
-                <p>Peso: </p>
-              </div>
-              <div>
-                <p>{Number(item.mass.kg).toLocaleString()} KG</p>
-              </div>
-            </div>     
-            <div className="list-infos list-infos-twodisplay">
-              <div>
-                <p>Descrição: </p>
-              </div>
-              <div>
-                <p className="text-description">{item.description} KG</p>
-              </div>
-            </div>                    
+            <div className="area-img" 
+              style={index % 2 !== 0 ? { paddingLeft: '30px' } : { paddingLeft: '' }}
+              data-aos={index % 2 !== 0 ? "fade-left" : "fade-right"}
+              >
+              <img src={item.flickr_images[0]} alt="Imagem do modelo de foguete" />
+            </div>
           </div>
-
-          <div className="area-img" style={index%2 !== 0 ? {paddingLeft: '30px'} : {paddingLeft: ''}}>
-            <img src={item.flickr_images[0]}  alt="Imagem do modelo de foguete"/>
-          </div>
-        </div>
-      ))}
+        ))}
     </div>
   )
 }
